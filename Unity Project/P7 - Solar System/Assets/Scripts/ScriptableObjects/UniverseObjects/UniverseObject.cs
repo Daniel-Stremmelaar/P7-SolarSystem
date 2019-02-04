a5d86b2befc79a7a69605fd6370cb245b8ee5587
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-02-04 11:32:21 +0100

    Made Scriptable Object for universe objects

---
+++ Unity Project/P7 - Solar System/Assets/Scripts/ScriptableObjects/UniverseObjects/UniverseObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Universe Object", menuName = "Universe Object")]
+public class UniverseObject : ScriptableObject {
+    [Header ("Basic")]
+
+    [Tooltip ("Name of the object")]
+    public new string name;
+
+    [Header ("Values")]
+
+    [Tooltip ("Mass of the object in kg")]
+    public float mass;
+
+    [Tooltip ("Radius of the object in km")]
+    public float radius;
+
+}
