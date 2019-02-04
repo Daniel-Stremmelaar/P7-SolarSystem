8e5965fcae4928a2875eccfe4431461f26ffd7e2
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-02-04 14:23:06 +0100

    Made the edit mode script a bit less performance heavy

--- Unity Project/P7 - Solar System/Assets/Scripts/UI/InformationBox.cs
+++ Unity Project/P7 - Solar System/Assets/Scripts/UI/InformationBox.cs
@@ -7,32 +7,35 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class InformationBox : MonoBehaviour {
     public UniverseObject currentObject;
+    public UniverseObject oldObject;
     public TextMeshProUGUI nameTextObject;
     public TextMeshProUGUI factTextObject;
     public TextMeshProUGUI massTextObject;
     public TextMeshProUGUI radiusTextObject;
     public TextMeshProUGUI rotationTextObject;
 
+    void Start () {
+        oldObject = currentObject;
+    }
+
     void Update () {
-        SetValues ();
+        if (oldObject != currentObject) {
+            SetValues ();
+            oldObject = currentObject;
+        }
     }
 
     void SetValues () {
         nameTextObject.text = currentObject.name;
         massTextObject.text = currentObject.mass.ToString ();
         radiusTextObject.text = currentObject.radius.ToString ();
-        rotationTextObject.text = currentObject.rotationLength.ToString();
+        rotationTextObject.text = currentObject.rotationLength.ToString ();
 
         string objectFact = "";
 
-        int index = Random.Range(0, currentObject.facts.Count);
+        int index = Random.Range (0, currentObject.facts.Count);
         objectFact = currentObject.facts[index];
 
         factTextObject.text = objectFact;
     }
-
-    void SetTextSize() {
-        
-    }
-
 }
