f21313ec1be96622cf396dab696024f601df8735
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-02-04 15:49:14 +0100

    Added quick toggle script

---
+++ Unity Project/P7 - Solar System/Assets/Scripts/Animate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    public bool toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(toggle) {
+            GetComponent<Animator>().SetTrigger("Toggle");
+        }
+    }
+}
