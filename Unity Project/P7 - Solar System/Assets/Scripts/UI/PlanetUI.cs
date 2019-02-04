e4d474db69cc2e3e9a0ce8c28cacccda05064f3c
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-02-04 10:45:37 +0100

    Imported TextMeshPro packages

--- Unity Project/P7 - Solar System/Assets/Scripts/UI/PlanetUI.cs
+++ Unity Project/P7 - Solar System/Assets/Scripts/UI/PlanetUI.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetUI : MonoBehaviour {
-    public void OnMouseOver () {
+    //What happens if mouse hovers over the planet
+    public void OnMouseEnter () {
+
+    }
+
+    //What happens if mouse stops hovering over the planet
+    public void OnMouseExit() {
+
+    }
+    
+    //What happens if mouse clicks on the planet
+    public void OnMouseClick() {
 
     }
 }
