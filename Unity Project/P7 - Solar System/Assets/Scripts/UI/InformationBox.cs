b7b9d981c7ae8c3a6d6cfd274082110b193918bc
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-02-04 11:45:56 +0100

    Added information box script

---
+++ Unity Project/P7 - Solar System/Assets/Scripts/UI/InformationBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class InformationBox : MonoBehaviour {
+    public UniverseObject currentObject;
+    public Transform nameTextObject;
+    public Transform massTextObject;
+    public Transform radiusTextObject;
+
+    void Update () {
+        SetValues();
+    }
+
+    void SetValues () {
+        nameTextObject.GetComponent<TextMeshProUGUI> ().text = currentObject.name;
+        massTextObject.GetComponent<TextMeshProUGUI> ().text = currentObject.mass.ToString ();
+        radiusTextObject.GetComponent<TextMeshProUGUI> ().text = currentObject.radius.ToString();
+
+    }
+
+}
