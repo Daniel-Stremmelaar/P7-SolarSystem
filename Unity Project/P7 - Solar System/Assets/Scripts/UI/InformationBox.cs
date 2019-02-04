5605428fb0562210296d959e2ac9856831df0a77
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-02-04 14:01:28 +0100

    Made facts show in the UI

--- Unity Project/P7 - Solar System/Assets/Scripts/UI/InformationBox.cs
+++ Unity Project/P7 - Solar System/Assets/Scripts/UI/InformationBox.cs
@@ -23,6 +23,13 @@ public class InformationBox : MonoBehaviour {
         radiusTextObject.text = currentObject.radius.ToString ();
         rotationTextObject.text = currentObject.rotationLength.ToString();
 
+        string objectFact = "";
+        
+        int index = Random.Range(0, currentObject.facts.Count);
+        objectFact = currentObject.facts[index];
+
+        factTextObject.text = objectFact;
+
 
     }
 
