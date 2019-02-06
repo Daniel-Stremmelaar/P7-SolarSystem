fbab4e07c75678a584ad3aa1bc1c3cf7de2eb5ec
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-02-06 15:42:09 +0100

    Waypoint system for planets
    woah

---
+++ Unity Project/P7 - Solar System/Assets/Scripts/Cam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 curCamVel = Vector3.zero;
+    [SerializeField]
+    float freeCamRotSpeed = 40;
+    [SerializeField]
+    float camAcceleration = 4;
+
+
+    void Start()
+    {
+        if(Application.isEditor == true){
+             Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void Update()
+    {
+        FreeCam();
+    }
+    void FreeCam()
+    {
+        curCamVel = Vector3.Lerp(curCamVel, new Vector3(-Input.GetAxis("Mouse Y") * freeCamRotSpeed, Input.GetAxis("Mouse X") * freeCamRotSpeed, 0), Time.deltaTime * camAcceleration);
+        transform.Rotate(curCamVel * Time.deltaTime);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+
+        transform.position += transform.TransformDirection(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetAxis("Vertical")) * Time.deltaTime * 10;
+
+    }
+}
---
+++ Unity Project/P7 - Solar System/Assets/Scripts/WayPoint.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[ExecuteInEditMode]
+
+public class WayPoint : MonoBehaviour
+{
+
+    LineRenderer line;
+    [SerializeField]
+    List<Vector3> points = new List<Vector3>();
+    int curFollowPoint = 0;
+    [SerializeField] Transform toMove;
+    [SerializeField] float speed = 10;
+    int lastPointCount = 0;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        SetPoints();
+        toMove.position = points[0];
+    }
+
+    void Update()
+    {
+        // SetPoints();
+        CheckPoints();
+        Follow(toMove);
+    }
+
+    void CheckPoints()
+    {
+        if (line.positionCount != lastPointCount)
+        {
+            SetPoints();
+        }
+        lastPointCount = line.positionCount;
+    }
+
+    void Follow(Transform follower)
+    {
+        follower.position = Vector3.MoveTowards(follower.position, points[curFollowPoint], Time.deltaTime * speed);
+        if (Vector3.Distance(follower.position, points[curFollowPoint]) < 0.3f)
+        {
+            if (curFollowPoint < points.Count - 1)
+            {
+                curFollowPoint++;
+            }
+            else
+            {
+                curFollowPoint = 0;
+            }
+        }
+    }
+
+    void SetPoints()
+    {
+        if (line == null)
+        {
+            line = GetComponent<LineRenderer>();
+        }
+        points.Clear();
+        for (int i = 0; i < line.positionCount; i++)
+        {
+            //  points.Add(transform.GetChild(i).transform.position);
+            points.Add(line.GetPosition(i));
+        }
+    }
+}
