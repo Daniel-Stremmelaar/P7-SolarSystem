aea61ed7533ecceb3b002d71b3e7dd02fcff9f6d
Admin <guy_meulen@live.nl>
2019-02-06 14:36:12 +0100

    Test Scene VR
    VR works, can show UI that looks at camera.

---
+++ Unity Project/P7 - Solar System/Assets/Scripts/UIDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDirection : MonoBehaviour
+{
+    private Transform playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookPos = playerCamera.position - transform.position;
+        transform.LookAt(lookPos);
+    }
+}
--- Unity Project/P7 - Solar System/Assets/Scripts/VRController.cs
+++ Unity Project/P7 - Solar System/Assets/Scripts/VRController.cs
@@ -7,10 +7,8 @@ using Valve.VR.InteractionSystem;
 public class VRController : MonoBehaviour
 {
     [Header ("VR Input")]
-    public SteamVR_Action_Boolean triggerLeft;
-    public SteamVR_Input_Sources inputSource1 = SteamVR_Input_Sources.LeftHand;
-    public SteamVR_Action_Boolean triggerRight;
-    public SteamVR_Input_Sources inputSource2 = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Action_Boolean trigger;
+    public SteamVR_Input_Sources inputSource;
     // Start is called before the first frame update
 
     private void OnEnable()
@@ -26,13 +24,6 @@ public class VRController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (triggerLeft.GetStateDown(inputSource1))
-        {
-            print("test");
-        }
-        if (triggerRight.GetStateDown(inputSource2))
-        {
-            print("succes");
-        }
+        
     }
 }
