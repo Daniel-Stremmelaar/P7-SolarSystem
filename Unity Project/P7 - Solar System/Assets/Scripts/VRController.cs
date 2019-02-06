881b6461f90e054f9ea76fe8c76aeacbf67cc232
Admin <guy_meulen@live.nl>
2019-02-06 13:15:43 +0100

    VR Basic Interaction
    We can print text on trigger pull

---
+++ Unity Project/P7 - Solar System/Assets/Scripts/VRController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class VRController : MonoBehaviour
+{
+    [Header ("VR Input")]
+    public SteamVR_Action_Boolean triggerLeft;
+    public SteamVR_Input_Sources inputSource1 = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Action_Boolean triggerRight;
+    public SteamVR_Input_Sources inputSource2 = SteamVR_Input_Sources.RightHand;
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerLeft.GetStateDown(inputSource1))
+        {
+            print("test");
+        }
+        if (triggerRight.GetStateDown(inputSource2))
+        {
+            print("succes");
+        }
+    }
+}
