1f7b2b5cf22b5b0eeffa299e2ed3e78bd263d819
Admin <guy_meulen@live.nl>
2019-02-07 11:22:26 +0100

    New Project in HDRP
    Basic controls work again.

---
+++ P7 - SSS - HDRP/Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanel : SelectableObject
+{
+    public float elementDistance;
+    //public GameObject controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        uiElementOffset = transform.forward * elementDistance;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        uiElement.transform.position = transform.position + uiElementOffset;
+        uiElement.transform.rotation.SetLookRotation(transform.up, transform.forward);
+        
+
+        //uiElement.transform.position = controller.transform.position + uiElementOffset;
+    }
+}
---
+++ P7 - SSS - HDRP/Assets/Scripts/InteractionController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class InteractionController : VRController
+{
+    [Header("Interaction")]
+    public GameObject interactText;
+    public int interactionRange;
+    private RaycastHit hit;
+    public LineRenderer line;
+    public List<Vector3> positions = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions[0] = transform.position;
+        line.SetPosition(0, positions[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positions[1] = transform.position + (transform.forward * interactionRange);
+        line.SetPosition(1, positions[1]);
+        Debug.DrawRay(transform.position, transform.forward * interactionRange);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactionRange))
+        {
+            print("hit");
+            if(hit.transform.gameObject.tag == "Interactable")
+            {
+                print("find");
+                if (interactText.activeSelf == false)
+                {
+                    interactText.SetActive(true);
+                }
+                if (trigger.GetStateDown(inputSource))
+                {
+                    print("activate");
+                }
+            }
+        }
+        else if (interactText.activeSelf == true)
+        {
+            interactText.SetActive(false);
+        }
+    }
+}
