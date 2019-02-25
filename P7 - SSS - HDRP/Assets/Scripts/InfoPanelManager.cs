40e1df75fef4da20ab994941481d9c3d6c433864
Admin <guy_meulen@live.nl>
2019-02-25 16:50:52 +0100

    VR Functions
    VR seems to work fine now, UI interaction is being worked on.

---
+++ P7 - SSS - HDRP/Assets/Scripts/InfoPanelManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanelManager : MonoBehaviour
+{
+    public GameObject panelTracker;
+    public int count;
+    public List<GameObject> panels = new List<GameObject>();
+    // Start is called before the first frame update
+    private void Start()
+    {
+        foreach (GameObject g in panels)
+        {
+            g.SetActive(false);
+        }
+        panelTracker.GetComponent<InfoPanel>().uiElement = panels[0].transform;
+        panels[0].SetActive(true);
+    }
+
+    public void SwitchPanel(int i)
+    {
+        count += i;
+        foreach (GameObject g in panels)
+        {
+            g.SetActive(false);
+        }
+        if (count > panels.Count - 1)
+        {
+            count = 1;
+        }
+        if (count < 1)
+        {
+            count = panels.Count - 1;
+        }
+        if (i == 0)
+        {
+            if (panels[i].active == true)
+            {
+                panelTracker.GetComponent<InfoPanel>().uiElement = panels[count].transform;
+                panels[count].SetActive(true);
+                panels[i].SetActive(false);
+            }
+            else
+            {
+                panelTracker.GetComponent<InfoPanel>().uiElement = panels[i].transform;
+                panels[i].SetActive(true);
+            }
+        }
+        else
+        {
+            panelTracker.GetComponent<InfoPanel>().uiElement = panels[count].transform;
+            panels[count].SetActive(true);
+        }
+    }
+}
---
+++ P7 - SSS - HDRP/Assets/Scripts/MenuController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class MenuController : VRController
+{
+    public SteamVR_Action_Vector2 trackPad;
+    public SteamVR_Action_Boolean trackPress;
+
+    public InfoPanelManager panelManager;
+    public GameObject player;
+    public GameObject systemMap;
+    public int mapDistance;
+    private Vector3 mapOffset;
+
+    public GameObject UIPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        systemMap.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trigger.GetStateDown(inputSource))
+        {
+            UIPanel.active = !UIPanel.active;
+        }
+
+        //trackpad axis: horizontal left number, vertical right number. Left/down = -1, right/up = 1
+
+        if (trackPad.GetAxis(inputSource) != new Vector2 (0,0))
+        {
+            print(trackPad.GetAxis(inputSource));
+        }
+
+        if(trackPad.GetAxis(inputSource).x > 0.7 && trackPress.GetStateDown(inputSource))
+        {
+            print("next");
+            panelManager.SwitchPanel(1);
+        }
+
+        if (trackPad.GetAxis(inputSource).x < -0.7 && trackPress.GetStateDown(inputSource))
+        {
+            print("next");
+            panelManager.SwitchPanel(-1);
+        }
+
+        if(trackPad.GetAxis(inputSource).y > 0.7 && trackPress.GetStateDown(inputSource))
+        {
+            panelManager.SwitchPanel(0);
+            /*systemMap.active = !systemMap.active;
+            mapOffset = player.transform.forward * mapDistance;
+            mapOffset.y = player.transform.position.y;
+            systemMap.transform.position = player.transform.position + mapOffset;
+            systemMap.transform.rotation = player.transform.rotation;
+            systemMap.transform.eulerAngles.Set(0, player.transform.eulerAngles.y, player.transform.eulerAngles.z);*/
+        }
+
+    }
+}
