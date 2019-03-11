b118653e5c675e857f42eea7849116fb38b31db9
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-03-11 09:29:16 +0100

    Timed Events Script
    If you activate it, it start coroutines for all events. Add them in the inspector.

---
+++ P7 - SSS - HDRP/Assets/TimedEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimedEvents : MonoBehaviour
+{
+    
+    [SerializeField] EventArray[] events;
+
+    public void StartEvents(){
+        float totalTime = 0;
+        for (int i = 0; i < events.Length; i++)
+        {
+            StartCoroutine(ActivateEvent(events[i].curEvent,events[i].waitTime + totalTime));
+            totalTime += events[i].waitTime;
+        }
+    }
+
+    IEnumerator ActivateEvent(UnityEvent ev,float waitTime){
+        yield return new WaitForSeconds(waitTime);
+        ev.Invoke();
+    }
+}
+
+[System.Serializable]
+public class EventArray
+{
+    public float waitTime = 1;
+    public UnityEvent curEvent;
+}
