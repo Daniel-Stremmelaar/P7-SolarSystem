cfdf089e07a184f3d686fa67ac35be69329e1c3c
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-04-03 09:10:01 +0200

    Grass and Stefan UI edit

---
+++ P7 - SSS - HDRP/Assets/Scripts/UI/InputEvent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class InputEvent : MonoBehaviour
+{
+    public EventArray[] timedEvents;
+    [SerializeField] string input = "Roll_P1";
+    [SerializeField] bool checkInUpdate = false;
+
+    public void CheckInput()
+    {
+        if (Input.GetButtonDown(input) == true && IsInvoking("IgnoreInput") == false)
+        {
+            float curTime = 0;
+            for (int i = 0; i < timedEvents.Length; i++)
+            {
+                curTime += timedEvents[i].waitTime;
+                StartCoroutine(CoroutineEvents(curTime, timedEvents[i].curEvent));
+                CancelInvoke("IgnoreInput");
+                Invoke("IgnoreInput", curTime);
+            }
+        }
+    }
+
+    IEnumerator CoroutineEvents(float time, UnityEvent ev)
+    {
+        yield return new WaitForSeconds(time);
+        ev.Invoke();
+    }
+
+    void IgnoreInput()
+    {
+        //invoke fucntion lol
+    }
+
+    void LateUpdate()
+    {
+        if (checkInUpdate == true)
+        {
+            CheckInput();
+        }
+    }
+}
---
+++ P7 - SSS - HDRP/Assets/SpawnSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSound : MonoBehaviour
+{
+    public void SpawnSoundEffect(AudioClip clip)
+    {
+        GameObject g = new GameObject();
+        g.AddComponent(typeof(AudioSource));
+        g.GetComponent<AudioSource>().clip = clip;
+        g.GetComponent<AudioSource>().Play();
+        Destroy(g, clip.length);
+    }
+}
