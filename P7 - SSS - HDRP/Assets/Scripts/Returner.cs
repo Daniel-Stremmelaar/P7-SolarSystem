b8b8b09f5d8717c310a40085bdd3475729e8a8ad
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-04-04 13:08:02 +0200

    Scene switch
    Can now switch between scenes as needed. (Does not yet work for galaxy scene)

---
+++ P7 - SSS - HDRP/Assets/Scripts/Returner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Returner : MonoBehaviour
+{
+    public Button back;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        back.onClick.AddListener(Back);
+    }
+
+    private void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
