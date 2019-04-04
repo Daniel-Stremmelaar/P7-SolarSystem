19c191eea06ace0091cf67d27ab33a6621b8e6fd
Remasu <stefanherder2001@gmail.com>
2019-04-04 14:36:32 +0200

    Added travel vfx script

---
+++ P7 - SSS - HDRP/Assets/TravelVFX/TravelFlash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TravelFlash : MonoBehaviour {
+
+    public AnimationCurve opacityOverTime;
+
+    GameObject panel;
+
+    public GameObject panelPrefab;
+
+    private float timer = 0f;
+
+    Color color;
+
+
+    public void Awake()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+
+        panel = Instantiate(panelPrefab,canvas.transform);
+
+        color = panel.GetComponent<Image>().color;
+    }
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+        color.a = opacityOverTime.Evaluate(timer);
+        panel.GetComponent<Image>().color = color;
+    }
+
+}
